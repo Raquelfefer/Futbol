7ec9f955f9beaf068b59825c1e58af8514e39ef9
Raquelfefer <raquelfefer@gmail.com>
2025-03-04 10:37:34 +0100

    jugador

--- src/equipo/jugadores/Jugador.java
+++ src/equipo/jugadores/Jugador.java
@@ -1,5 +1,65 @@
 package equipo.jugadores;
 
-public class Jugador {
+public abstract class Jugador {
 
+	private int idJugador;
+	private String nombre;
+	private int dorsal;
+	private String equipo;
+	
+	private static int ultimoJugador = 1;
+	
+	public Jugador(String nombre, int dorsal, String equipo) {
+		this.idJugador = ultimoJugador++;
+		setNombre(nombre);
+		setDorsal(dorsal);
+		setEquipo(equipo);
+	}
+	
+	public int getIdJugador() {
+		return this.idJugador;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public int getDorsal() {
+		return this.dorsal;
+	}
+	
+	public String getEquipo() {
+		return this.equipo;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setDorsal(int dorsal) {
+		this.dorsal = dorsal;
+	}
+	
+	public void setEquipo(String equipo) {
+		this.equipo = equipo;
+	}
+	
+	public abstract void mostrarDatos();
+
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + nombre + ", dorsal=" + dorsal + ", equipo=" + equipo + "]";
+	}
+	
+	@Override
+	public boolean equals(Object otro) {
+		Jugador otroJugador = (Jugador) otro;
+		boolean iguales;
+		if(this.idJugador == otroJugador.idJugador) {
+			iguales = true;
+		}else {
+			iguales = false;
+		}
+		return iguales;
+	}
 }
